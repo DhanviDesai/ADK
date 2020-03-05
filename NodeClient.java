6a0426e6d567d9a22433b7f96ce85801f03da8a2
Dhanvi Desai <dhanvidesai99@gmail.com>
2020-03-05 21:27:50 +0530

    Delete NodeClient.java

--- NodeClient.java
+++
@@ -1,6 +0,0 @@
-public class NodeClient{
-public static void main(String args[]){
-//The node talks to tracker and gets the updated peer list
-//The node then connects to the specific node and starts communication
-}
-}
