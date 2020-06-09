1e9a5601c89618e33e6fe2424dea4b0c0eba98b7
DhanviDesai <dhanvidesai99@gmail.com>
2020-06-09 09:26:13 +0530

    Interrings 3.8.4,Distri-core 1.11,UI 1

--- NodePeer/codeEditADK.js
+++ NodePeer/codeEditADK.js
@@ -27,3 +27,28 @@ $('#runButton').on('click',(e)=>{
   });
   */
 });
+var allTogether = ["VAR","FUNCTION","RETURN","FOR","WHILE","IF","ELSE","ELSE IF","SEND","RECV","PRINT","SIZE","RANK"];
+var keywords = ["FUNCTION","RETURN","FOR","WHILE","IF","ELSE","ELSE IF"];
+var functions = ["SEND","RECV","PRINT","SIZE","RANK"];
+var regexFromMyArray = new RegExp(allTogether.join("|"), 'ig');
+//var regExOwnFunctions = new RegExp(ownFunctions.join("|"),'ig');
+$('#codeEditor').keyup(function(event){
+  document.getElementById('codeEditorDummy').innerHTML = $('#codeEditor').html().replace(regexFromMyArray,function(str){
+    if(functions.indexOf(str.trim().toUpperCase())>-1){
+      return '<span class="ownHighlight">'+str+'</span>';
+    }else if(str.trim().toUpperCase() == "VAR"){
+      return '<span class="varHighlight">'+str+'</span>';
+    }else{
+      return '<span class="highlighted">'+str+'</span>';
+    }
+  });
+  /*document.getElementById('codeEditorDummy').innerHTML = $('#codeEditor').html().replace(regExOwnFunctions,(str)=>{
+    return '<span class="ownHighlight">'+str+'</span';
+  });*/
+
+});
+var target = $("#codeEditorDummy");
+  $("#codeEditor").scroll(function() {
+    target.prop("scrollTop", this.scrollTop)
+          .prop("scrollLeft", this.scrollLeft);
+  });
