4e1bed1a1653a963c3e87948582f943f2aec5b20
Keeeee <keeeee@pop-os.localdomain>
2020-06-13 00:59:03 +0530

    Interrings arch 3.8.6-5.23, Distri-core 1.14.17.7, UI 3.1

---
+++ NodePeer/DrawChart.js
@@ -0,0 +1,58 @@
+var ctx = document.getElementById("myChart").getContext('2d');
+
+function handleUpdate(chart,label,data){
+  chart.data.datasets.forEach((dataset, i) => {
+    dataset.data.pop();
+    dataset.data.pop();
+    dataset.data.pop();
+  });
+  chart.data.datasets.forEach((dataset, i) => {
+    dataset.data.push(data[0]);
+    dataset.data.push(data[1]);
+    dataset.data.push(data[2]);
+  });
+  chart.update(0);
+}
+var myDoughnutChart;
+function firstChart(){
+  myDoughnutChart = new Chart(ctx,{
+    type:'doughnut',
+    data:{
+      datasets:[{
+        data:[10,20,30],
+        backgroundColor:[
+          'rgba( 125, 206, 160 ,1)',
+          'rgba( 244, 208, 63 ,1)',
+          'rgba( 243, 156, 18 ,1)'
+        ]
+      }],
+      labels:[
+        'free',
+        'buff',
+        'cache'
+      ]
+    },
+  });
+  setTimeout(() => {
+    drawChart();
+  },820);
+}
+function drawChart(){
+  var cp1 = childProcess.exec("vmstat | sed -n '3p' | awk '{print $4,$5,$6}'",(err,stdout,stderr) => {
+    var resourceLine = stdout;
+    var resources = resourceLine.split(" ");
+    var free = parseInt(resources[0]);
+    var buff = parseInt(resources[1]);
+    var cache = parseInt(resources[2]);
+    var labels = ['free','buff','cache'];
+    var dataList = [];
+    dataList.push(free);
+    dataList.push(buff);
+    dataList.push(cache);
+    handleUpdate(myDoughnutChart,labels,dataList);
+  });
+  setTimeout(() => {
+    drawChart();
+  },1000);
+}
+firstChart();
